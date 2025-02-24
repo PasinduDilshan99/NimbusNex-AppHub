0cc81c82f3e6a7f8414b870594dfe96d509be62d
Pasindu Dilshan <pd.dimbulana@gmail.com>
2025-02-24 22:32:56 +0530

    add messages

--- src/main/java/com/nimbusnex/appHub/util/Constant.java
+++ src/main/java/com/nimbusnex/appHub/util/Constant.java
@@ -1,4 +1,9 @@
 package com.nimbusnex.appHub.util;
 
 public class Constant {
+
+    private Constant(){}
+
+    public static final String STARS = "*****************************************************";
+
 }
--- src/main/java/com/nimbusnex/appHub/util/ResponseMessagesAndCodes.java
+++ src/main/java/com/nimbusnex/appHub/util/ResponseMessagesAndCodes.java
@@ -1,4 +1,29 @@
 package com.nimbusnex.appHub.util;
 
 public class ResponseMessagesAndCodes {
+
+    private ResponseMessagesAndCodes() {
+    }
+
+    public static final String SUCCESS_STATUS = "SUCCESS";
+    public static final Integer SUCCESS_CODE = 200;
+    public static final String SUCCESS_MESSAGE = "Successful";
+
+    public static final String CREATED_STATUS = "CREATED";
+    public static final Integer CREATED_CODE = 201;
+    public static final String CREATED_MESSAGE = "Resource created successfully";
+
+    public static final String ACCEPTED_STATUS = "ACCEPTED";
+    public static final Integer ACCEPTED_CODE = 202;
+    public static final String ACCEPTED_MESSAGE = "Request accepted but not yet processed";
+
+    public static final String NO_CONTENT_STATUS = "NO_CONTENT";
+    public static final Integer NO_CONTENT_CODE = 204;
+    public static final String NO_CONTENT_MESSAGE = "No content available";
+
+    public static final String UNSUCCESS_STATUS = "UNSUCCESS";
+    public static final Integer UNSUCCESS_CODE = 400;
+    public static final String UNSUCCESS_MESSAGE = "Unsuccessful";
+
+
 }
